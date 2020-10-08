b88cbddca0fb669f798b64b699e669373179b43b
Waiyan Myothet <menooby1996@gmail.com>
2020-10-08 15:51:50 +0630

    Add project files.

---
+++ GoogleKeepClone-API/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace GoogleKeepClone_API.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ GoogleKeepClone-API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GoogleKeepClone_API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
